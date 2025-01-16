62eb0335fc82913519cd09e8db3f53e0e9fde0a1
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-01-16 22:19:57 +0530

    add form for create event

--- events/forms.py
+++ events/forms.py
@@ -3,7 +3,17 @@ from .models import Event
 
 
 class EventCreationForm(forms.ModelForm):
+
+    date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+    time = forms.TimeField(widget=forms.TimeInput(attrs={'type': 'time'}))
+
     class Meta:
         model=Event
         fields=["title", "description", "date", "time", "location", "max_participants"]
+
+
+    def clean(self):
+        cleaned_data = super().clean()
+
+        return cleaned_data
 
--- events/views.py
+++ events/views.py
@@ -6,8 +6,13 @@ from .forms import EventCreationForm
 # Create your views here.
 
 def create_event(request):
-    form = EventCreationForm()
-
-    return render(request, 'events/events.html', {'form': form})
+    if request.method == 'POST':
+        form = EventCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse("Event created successfully")
+    else:
+        form = EventCreationForm()
+        return render(request, 'events/create.html', {'form': form})
 
 
